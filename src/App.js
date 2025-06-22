1323c21131d660ae10b3bbe066e4f415b9e0c86d
kashish7-7 <kashishhere003@gmail.com>
2025-06-22 10:16:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,21 +7,17 @@ import Register from './assets/components/Register';
 import HowItWorks from './assets/components/HowItWorks';
 
 const BACKEND_URL = "https://validify-backend.vercel.app";
-// You can also use environment variable like:
-// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 function App() {
   return (
     <Router>
       <Header />
-
       <Routes>
         <Route path="/home" element={<Home backendUrl={BACKEND_URL} />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
         <Route path="/register" element={<Register backendUrl={BACKEND_URL} />} />
         <Route path="*" element={<Home backendUrl={BACKEND_URL} />} />
       </Routes>
-
       <Footer />
     </Router>
   );
